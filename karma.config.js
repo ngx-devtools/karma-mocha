f14af9d151789111826f37effa2a927251cbd20d
Jay <aelbore.sg@gmail.com>
2019-02-18 17:54:23 +0800

    Update karma.config.js

--- karma.config.js
+++ karma.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
+
 const typescript2 =  require('rollup-plugin-typescript2')
 const nodeResolve = require('rollup-plugin-node-resolve')
+const istanbul = require('rollup-plugin-istanbul')
 
 module.exports = function(config) {
   config.set({
@@ -37,17 +39,38 @@ module.exports = function(config) {
           cacheRoot: path.join(path.resolve(), 'node_modules/.tmp/.rts2_cache'), 
           useTsconfigDeclarationDir: true          
         }),
-        nodeResolve()
+        nodeResolve(),
+        istanbul({
+          exclude: [
+            "src/**/*.spec.ts",
+            "node_modules/**/*"
+          ]
+        })
       ],
       output: {
         format: "iife", 
         sourcemap: 'inline'
       }
     },
+
+    coverageIstanbulReporter: {
+      reports: [ 'lcov', 'text-summary' ],
+      dir: 'coverage',
+      combineBrowserReports: true,
+      skipFilesWithNoCoverage: true,
+      thresholds: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80,
+        }
+      }
+    },
   
     browsers: ['ChromeHeadlessNoSandbox'],
   
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage-istanbul'],
   
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
